da954764fb88972ec7db95f8d71ab037d8dc6d2f
Jonathan Meyer <25950837+MeyerJon@users.noreply.github.com>
2025-11-13 17:38:02 +0100

    Updating scripts

--- agent.py
+++ agent.py
@@ -3,4 +3,4 @@ from SpaceTraders.controllers import system_market_intel as MI
 import asyncio
 
 if __name__ == "__main__":
-    asyncio.run(MI.maintain_tradegood_data('X1-QT34', 5, mode="no_exchanges"))
+    asyncio.run(MI.maintain_tradegood_data('X1-QT34', 60*5, mode="no_exchanges"))
